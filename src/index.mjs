fc4830b0df34663b6c64463a908f59cf6714b057
jascha <jascha@jaeh.at>
2020-04-26 13:21:11 +0200

    use https

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,7 @@
-import http from 'http'
+import https from 'https'
+import path from 'path'
 
-import { log, middleware } from '@grundstein/commons'
+import { fs, log, middleware } from '@grundstein/commons'
 
 import handler from './handler.mjs'
 
@@ -8,10 +9,23 @@ export const run = async (config = {}) => {
   try {
     const startTime = log.hrtime()
 
-    const { dir = '/var/www/html', host = '0.0.0.0', port = 2350 } = config
+    const {
+      dir = path.join('/var', 'www', 'html'),
+      host = 'gms.grund.stein',
+      port = 2350,
+      certDir = path.join('/root', 'ca', 'intermediate'),
+    } = config
+
+    const keyFile = path.join(certDir, 'private', `${host}.key.pem`)
+    const certFile = path.join(certDir, 'certs', `${host}.cert.pem`)
+
+    const options = {
+      key: await fs.readFile(keyFile),
+      cert: await fs.readFile(certFile),
+    }
 
     const worker = await handler(dir)
-    const server = http.createServer(worker)
+    const server = https.createServer(options, worker)
 
     const clientError = middleware.clientError({ host, port, startTime })
     server.on('clientError', clientError)
