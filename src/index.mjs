e8972ece304d46e4a8ca9e0cfd94ff26f2fe75f0
jascha <jascha@jaeh.at>
2020-04-26 13:42:59 +0200

    src/index.mjs: use getHostCertificate from commons, simplify

--- src/index.mjs
+++ src/index.mjs
@@ -3,37 +3,24 @@ import path from 'path'
 
 import { fs, log, middleware } from '@grundstein/commons'
 
+import { getHostCertificates } from '@grundstein/commons/lib.mjs'
+
 import handler from './handler.mjs'
 
 export const run = async (config = {}) => {
   try {
     const startTime = log.hrtime()
 
-    const {
-      dir = path.join('/var', 'www', 'html'),
-      host = 'gms.grund.stein',
-      port = 2350,
-      certDir = path.join('/root', 'ca', 'intermediate'),
-    } = config
+    const options = await getHostCertificates(config)
 
-    const keyFile = path.join(certDir, 'private', `${host}.key.pem`)
-    const certFile = path.join(certDir, 'certs', `${host}.cert.pem`)
-    const caCertFile = path.join(certDir, 'certs', 'intermediate.cert.pem');
-
-    const options = {
-      key: await fs.readFile(keyFile),
-      cert: await fs.readFile(certFile),
-      ca: await fs.readFile(caCertFile),
-    }
-
-    const worker = await handler(dir)
+    const worker = await handler(config.dir)
     const server = https.createServer(options, worker)
 
-    const clientError = middleware.clientError({ host, port, startTime })
+    const clientError = middleware.clientError({ ...config, startTime })
     server.on('clientError', clientError)
 
-    const listener = middleware.listener({ host, port, startTime })
-    server.listen(port, host, listener)
+    const listener = middleware.listener({ ...config, startTime })
+    server.listen(config.port, config.host, listener)
   } catch (e) {
     log.error(e)
     process.exit(1)
